cf4bbeb02bca0ea8d24387b11d175d06d68b20f6
meirrrrrr <potatothekartoshka@gmail.com>
2019-07-31 17:06:12 +0600

    favicon was added, and animations on mobile devices was disabled

--- src/main.js
+++ src/main.js
@@ -4,27 +4,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './main.scss'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faYoutube, faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import AOS from 'aos'
-import 'aos/dist/aos.css'
 import VueFullPage from 'vue-fullpage.js'
 
 if (screen.width > 768) {
   Vue.use(VueFullPage)
+  require('aos/dist/aos.css')
 }
-
-AOS.init()
-
-library.add(faYoutube, faInstagram, faFacebookF)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  el: '#app',
+  created () {
+    AOS.init()
+  },
   render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -468,9 +468,6 @@ export default {
     img {
       width: 2.25rem;
     }
-    /*h5 {*/
-    /*  width: 65% !important;*/
-    /*}*/
     width: 60%;
   }
 
@@ -479,27 +476,6 @@ export default {
   }
 
   @media screen and (max-width:768px) {
-
-    [data-aos] {
-      /*CSS transitions*/
-      -o-transition-property: none !important;
-      -moz-transition-property: none !important;
-      -ms-transition-property: none !important;
-      -webkit-transition-property: none !important;
-      transition-property: none !important;
-      /*CSS transforms*/
-      -o-transform: none !important;
-      -moz-transform: none !important;
-      -ms-transform: none !important;
-      -webkit-transform: none !important;
-      transform: none !important;
-      /*CSS animations*/
-      -webkit-animation: none !important;
-      -moz-animation: none !important;
-      -o-animation: none !important;
-      -ms-animation: none !important;
-      animation: none !important;
-    }
 
     #home {
       width: 100%;
